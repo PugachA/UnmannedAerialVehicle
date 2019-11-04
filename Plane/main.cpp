9d821d678c66d99fc23077dd0f83fc20a66d9f9f
NoMonsters <mlx-01@yandex.ru>
2019-11-04 12:17:54 +0300

    Добавил код для управления сервой
    В данном примере попробовал управлять сервой по прерываниям с кнопк. Каждое нажатие немного прибавляет или убавляет скважность.

--- Plane/main.cpp
+++ Plane/main.cpp
@@ -1,13 +1,56 @@
 #include "mbed.h"
+#include "rtos.h"
 
-DigitalOut led1(LED1);
+PwmOut red_led(LED1);
+InterruptIn button_up(PA_3);
+InterruptIn button_down(PA_5);
+
+uint32_t const BUTTON_DOWN = 1 << 0;
+uint32_t const BUTTON_UP = 1 << 1;
+
+Thread led_control_th;
+
+EventFlags buttons;
+
+float limiter(float max, float min, float value)
+{
+    return value >= max ? max : value <= min ? min : value;
+}
+void buttonUpHandler()
+{
+    buttons.set(BUTTON_UP);
+}
+void buttonDownHandler()
+{
+    buttons.set(BUTTON_DOWN);
+}
+void setLedOutPwm()
+{  
+    float duty_cycle{0};
+    uint32_t button_read_flag{0};
+
+    while(1){
+        button_read_flag = buttons.wait_any(BUTTON_UP | BUTTON_DOWN);
+        switch (button_read_flag)
+        {
+            case BUTTON_UP: red_led.pulsewidth_us(duty_cycle = limiter(1875, 900, duty_cycle + 100)); break;
+            case BUTTON_DOWN: red_led.pulsewidth_us(duty_cycle = limiter(1875, 900, duty_cycle - 100)); break;  
+            
+            default: break;
+        }
+        ThisThread::sleep_for(5);
+    }
+}
 
 int main()
 {
+    red_led = 0;
 
-    while(true)
-    {
-        led1 = !led1;
-        wait(2);
-    }
+    button_up.mode(PullDown);
+    button_down.mode(PullDown);
+
+    button_down.rise(&buttonDownHandler);
+    button_up.rise(&buttonUpHandler);
+    
+    led_control_th.start(setLedOutPwm);
 }
