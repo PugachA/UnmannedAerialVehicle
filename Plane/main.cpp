cd4b1b4413ea76e582f3f23303b312ea95acb528
NoMonsters <mlx-01@yandex.ru>
2019-11-05 18:32:15 +0300

    Сборка Plane
    Внес все изменения из getting_started в plane теперь всё собирается

--- Plane/main.cpp
+++ Plane/main.cpp
@@ -1,5 +1,6 @@
 #include "mbed.h"
 #include "rtos.h"
+#include "Math/Math.h"
 
 PwmOut red_led(LED1);
 InterruptIn button_up(PA_3);
@@ -12,10 +13,10 @@ Thread led_control_th;
 
 EventFlags buttons;
 
-float limiter(float max, float min, float value)
+/*float limiter(float max, float min, float value)
 {
     return value >= max ? max : value <= min ? min : value;
-}
+}*/
 void buttonUpHandler()
 {
     buttons.set(BUTTON_UP);
@@ -33,8 +34,8 @@ void setLedOutPwm()
         button_read_flag = buttons.wait_any(BUTTON_UP | BUTTON_DOWN);
         switch (button_read_flag)
         {
-            case BUTTON_UP: red_led.pulsewidth_us(duty_cycle = limiter(1875, 900, duty_cycle + 100)); break;
-            case BUTTON_DOWN: red_led.pulsewidth_us(duty_cycle = limiter(1875, 900, duty_cycle - 100)); break;  
+            case BUTTON_UP: red_led.pulsewidth_us(duty_cycle = Math::limiter<float>(1875, 900, duty_cycle + 100)); break;
+            case BUTTON_DOWN: red_led.pulsewidth_us(duty_cycle = Math::limiter<float>(1875, 900, duty_cycle - 100)); break;  
             
             default: break;
         }
--- Plane/mbed-os_build.bat
+++ Plane/mbed-os_build.bat
@@ -1,16 +1,15 @@
 @echo off 
-SET mbed-os_dir=%userprofile%\src\arm\mbed\mbed-os
-SET project_name=Plane
+SET project_name=Getting_started
 SET project_dir=%userprofile%\Documents\Projects\UnmannedAerialVehicle\%project_name%
-SET board=DISCO_L476VG
-SET toolchain=GCC_ARM
+::SET board=DISCO_L476VG
+::SET toolchain=GCC_ARM
 
-mbed config -G MBED_OS_DIR %mbed-os_dir%
 IF "%1" == "debug" (SET buildprofile=debug)
 IF "%1" == "release" (SET buildprofile=release)
-
-mbed compile -t %toolchain% ^
--m %board% --source . ^
---source %mbed-os_dir% ^
---profile %mbed-os_dir%\tools\profiles\%buildprofile%.json ^
+cd ../
+mbed compile ^
+--source %project_dir% ^
+--source BUILDS\mbed-os-build ^
+--source libraries ^
+--profile mbed-os\tools\profiles\%buildprofile%.json ^
 --build %project_dir%\..\BUILDS\BUILD_%project_name%\%buildprofile%
