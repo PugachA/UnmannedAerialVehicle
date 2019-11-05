a3881ff6428c9e4b16fe17076b4540e887b66883
NoMonsters <mlx-01@yandex.ru>
2019-11-05 11:31:51 +0300

    Просто main
    Подключили пароччку библиотек

--- Getting_started/main.cpp
+++ Getting_started/main.cpp
@@ -1,12 +1,14 @@
+#include "Sum\sum.h"
 #include "mbed.h"
 
 DigitalOut led1(LED1);
 
 int main()
 {
-
+	int result{0};
     while(true)
     {
+        result = sum(10, 12);
         led1 = !led1;
         wait(2);
     }
