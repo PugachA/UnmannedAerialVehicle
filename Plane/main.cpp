e9b628375d0c435e231fa4b6f2d439f99eb3dc78
Gavriliyuk Vasiliy <mlx-01@yandex.ru>
2020-02-23 15:24:28 +0300

    Убрал мусор
    Убрал мусор из когда, только участки комментов. Теперь ветка готова для ответвлений для проверки гипотиз почему странная ошибка в выводе возникает.

--- Plane/main.cpp
+++ Plane/main.cpp
@@ -19,17 +19,11 @@ Mail<int, 16> mail_box;
 class RcChannels
 {
 private:
-    //static EventFlags rc_channels; // очередь флагов для всех каналов(объектов) одна (вроде и не нужна когда есть очередь событий)
-    //uint32_t PIN_UP_FLAG;
-    //uint32_t PIN_DOWN_FLAG;
-    //bool const start{1};
-    //bool const stop{0};
     bool stop_timer{0};
     InterruptIn pin;
     Timer timer;
     int pulse_width;
     //Event<void()> start_timer  = queue.event(this, &RcChannels::calcPulseWidth, start);
-    Event<void()> save_pulse_width  = queue.event(this, &RcChannels::savePulseWidth);
     
     void upHandler();
     void downHandler();
@@ -43,26 +37,12 @@ public:
 
 void RcChannels::upHandler()
 {
-    //rc_channels.set(this->PIN_UP_FLAG);
-    //тут будет функция которая ставит в очередь старт таймера для этого канала
-    //Event<void(bool const)>    e  = queue.event(this, &Channels::calcPulseWidth);
-    //start_timer.post();
-    //red_led = 1;
     timer.start();
-    //start_or_stop_timer = 1;
 }
 void RcChannels::downHandler()
 {
-    //rc_channels.set(this->PIN_DOWN_FLAG);
-    //тут будет функция которая ставит в очередь стоп таймера для этого канала
-    //stop_timer.post();
     timer.stop();
     stop_timer = 1;
-    //save_pulse_width.post();
-    //pulse_width = timer.read_us();    
-    //timer.reset();
-    //red_led = 0;
-    //start_or_stop_timer = 0;
 }
 void RcChannels::savePulseWidth()
 {
@@ -130,30 +110,4 @@ int main()
         mail_box.free(mail);
 
     }
-    //ВОТ ЭТОТ КУСОК РАБОТАЕТ НЕ ТРОГАЙ
-
-    /*Serial pc(USBTX, USBRX);
-    red_led = 0;
-
-    EventQueue queue;
-    Timer t;
-    int timer_value{0};
-
-    Event<void(Timer*)> start_timer  = queue.event(&upHandler);
-    Event<void(Timer*)> stop_timer  = queue.event(&downHandler);
-    Event<void(DigitalOut)> blink  = queue.event(&ledBlink);
-
-    radio_control_th.start(callback(&queue, &EventQueue::dispatch_forever));
-    ThisThread::sleep_for(1000);
-    blink.post(red_led);
-    while(1)
-    {
-        //red_led = !red_led;
-        start_timer.post(&t);
-        ThisThread::sleep_for(1000);
-        timer_value = t.read_ms();
-        printf("%d\n", timer_value);
-        stop_timer.post(&t);
-        t.reset();
-    }*/
 }
