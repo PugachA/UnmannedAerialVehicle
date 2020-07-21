d5a9e5b5091bb3f01c8b164be4cdc14bc0ef5453
NoMonsters <33801216+NoMonsters@users.noreply.github.com>
2020-07-21 15:40:01 +0300

    [Радиоуправление] Прямое управление. Управление сервами. Чтение радио (#52)
    * Добавил класс для серв
    
    В конструкторе создаем по имени пина серву. И выставляем значение по скважности в мкС
    
    * Добавил пины для серв
    
    Добавил пины для сер, убрал мусор
    
    * Оптимизировал для тестов
    
    Убрал подтяжку ноги прерывания к гнд
    Убрал лишние пины, оставил канал газа
    Пофиксил тип входного значения в функции установки позиции серво
    
    * Убрал мбед ос
    
    Убрал поддержку мбед ос, код для нее. Теперь он Deprecated
    
    * Убрал обучение для mbedos
    
    Убрал поддержку мбедос, соответственно туториал не нужен
    
    * Текущая реализация прямого управ
    
    Прямой режим с армингом и взведением САС
    
    * Удалил проект на mbed
    
    Убрал стартовый проект на mbed, убрал чтение длины ШИМ имальсов на mbed и всяческие файлы ОС. Т.к. планируется реалзиция на HAL, мб в дальнейшем FreeRTOS
    
    * Обновил игнор
    
    Добавил метаданные куба в игнор
    
    * Перенес класс для радио приемника в отдельный файл
    
    Пока что перенес просто в папку inc так же внутри проекта куба, теперь надо разобраться как добавить это просто в общую папку библиотек
    
    * Вынес servo и rcchannel в отдельные файлы
    
    Получилось вынести только в папку inc внутри проекта, внешние к проектной папки работают странно, пока не разобрался. К тому же в либах есть включения некоторыз hal файлов, некорректно шарить из между проекутами в таком случае, т.к. либа всегда будет брать из какого-то конкретного проекта hal файлы
    
    * Вынес Beeper в отдельный файл
    
    Вынес бипер так же в inc внутри проекта. Косметика в main
    
    * Заменил публичный метод на приватную переменную
    
    * Убрал папку debug
    
    * Добавил Debug и Release в игнор

---
+++ Plane/Core/Inc/Beeper/Beeper.h
@@ -0,0 +1,23 @@
+#ifndef BEEPER_H
+#define BEEPER_H
+
+#include "stm32f4xx_hal.h"
+
+class Beeper
+{
+	private:
+		GPIO_TypeDef * port = 0;
+		uint32_t pin = 0;
+		const uint16_t LONG_BEEP_TIME_MS = 1000;
+		const uint16_t SHORT_BEEP_TIME_MS = 100;
+
+	public:
+		Beeper(GPIO_TypeDef * port, uint32_t pin);
+		~Beeper();
+		void shortBeep();
+		void longBeep();
+		void seriesBeep();
+
+};
+
+#endif // BEEPER_H
