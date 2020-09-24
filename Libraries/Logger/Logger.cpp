2111d46e24287fe0cce33ee91f9d4d2b5e806e3a
Pugach Alexander <fotone97@yandex.ru>
2020-09-24 18:37:25 +0300

    Добавлено хранение сообщение при ошибках записи

--- Libraries/Logger/Logger.cpp
+++ Libraries/Logger/Logger.cpp
@@ -18,6 +18,7 @@ Logger::Logger(const char* loggerName,
 	this->successPin = successPin;
 	this->errorGPIO = errorGPIO;
 	this->errorPin = errorPin;
+	this->errorCount = 0;
 
 	char buf[100];
 	sprintf(buf, "log-file-%s.log", this->loggerName);
@@ -37,6 +38,7 @@ Logger::Logger(const char* fileName,
 	this->successPin = successPin;
 	this->errorGPIO = errorGPIO;
 	this->errorPin = errorPin;
+	this->errorCount = 0;
 
 	CreateLogFile(fileName);
 }
@@ -58,12 +60,16 @@ void Logger::CreateLogFile(const char* fileName)
 		if(result == FR_OK)
 			this->SuccessMonitor();
 		else
-			this->ErrorMonitor();
+			this->ErrorMonitor("");
 	}
 }
 
-void Logger::ErrorMonitor()
+void Logger::ErrorMonitor(const char* message)
 {
+	strcat(this->errorQueue, message);
+	strcat(this->errorQueue, "\n");
+	this->errorCount++;
+
 	this->fileManager.UnMountSD();
 	FRESULT result = this->fileManager.MountSD();
 
@@ -80,40 +86,52 @@ void Logger::SuccessMonitor()
 	HAL_GPIO_WritePin(this->errorGPIO, this->errorPin, GPIO_PIN_RESET);
 }
 
-void Logger::Info(const char* message)
+void Logger::WriteToLog(const char* message, const char* messageType)
 {
 	uint32_t bufferSize = 200 + strlen(message);
 	char buffer[bufferSize];
 	sprintf(buffer, "{ \"timestamp\": \"%lu\", \"logger\": \"%s\", \"level\": \"%s\", \"message\": \"%s\" }",
 			HAL_GetTick(),
 			this->loggerName,
-			"Info",
+			messageType,
 			message);
 
-	int bytesWritten = this->fileManager.AppendLineToFile(this->filePath, buffer, false);
+	int bytesWritten;
+	if(this->errorCount > 0)
+	{
+		bytesWritten = this->fileManager.AppendLineToFile(this->filePath, this->errorQueue, false);
+
+		if(bytesWritten != -1)
+		{
+			memset(this->errorQueue, 0, sizeof(this->errorQueue));
+			this->errorCount = 0;
+		}
+	}
+
+	bytesWritten = this->fileManager.AppendLineToFile(this->filePath, buffer, false);
 
 	if(bytesWritten != -1)
 		this->SuccessMonitor();
 	else
-		this->ErrorMonitor();
+		this->ErrorMonitor(buffer);
+}
+
+void Logger::Info(const char* message)
+{
+	WriteToLog(message, "Info");
 }
 
 void Logger::Error(const char* message)
 {
-	uint32_t bufferSize = (200 + strlen(message)) * sizeof(char);
-	char *buffer = new char(bufferSize);
-	sprintf(buffer, "{ \"timestamp\": \"%lu\", \"logger\": \"%s\", \"level\": \"%s\", \"message\": \"%s\" }",
-			HAL_GetTick(),
-			this->loggerName,
-			"Error",
-			message);
+	WriteToLog(message, "Error");
+}
 
-	int bytesWritten = this->fileManager.AppendLineToFile(this->filePath, buffer, false);
+void Logger::Warn(const char* message)
+{
+	WriteToLog(message, "Warn");
+}
 
-	if(bytesWritten != -1)
-		this->SuccessMonitor();
-	else
-		this->ErrorMonitor();
-
-	delete[] buffer;
+void Logger::Debug(const char* message)
+{
+	WriteToLog(message, "Debug");
 }
