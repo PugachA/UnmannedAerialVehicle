67b7950da0465f6643b82ec57b14c12ce843e94c
Gavriliyuk Vasiliy <mlx-01@yandex.ru>
2020-10-05 13:30:47 +0300

    Добавил опрос барометра
    Читает сырую высоту с барометра и выводит в юарт.
    Важно: долго тразался, нельязя инитить баро до инита i2c

--- Plane/Core/Src/main.cpp
+++ Plane/Core/Src/main.cpp
@@ -26,6 +26,7 @@
 #include "RcChannel/RcChannel.h"
 #include "Servo/Servo.h"
 #include "Beeper/Beeper.h"
+#include "Baro/MS5611.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -73,6 +74,7 @@ static void MX_ADC2_Init(void);
 static void MX_I2C3_Init(void);
 static void MX_TIM6_Init(void);
 /* USER CODE BEGIN PFP */
+
 uint8_t Armed(Beeper* beeper)
 {
 	static uint8_t arm_flag = 0;
@@ -113,10 +115,15 @@ uint8_t ERS()
 	}
 	return ers_flag;
 }
+
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+//--------------------DEBUG DEFINES-----------------------
+
+//#define SERVO_DEBUG_UART
+#define SENSOR_DEBUG_UART
 
 /* USER CODE END 0 */
 
@@ -126,39 +133,45 @@ uint8_t ERS()
   */
 int main(void)
 {
-  /* USER CODE BEGIN 1 */
+	/* USER CODE BEGIN 1 */
 
-  /* USER CODE END 1 */
+	/* USER CODE END 1 */
 
-  /* MCU Configuration--------------------------------------------------------*/
+	/* MCU Configuration--------------------------------------------------------*/
 
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-  HAL_Init();
+	/* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+	HAL_Init();
 
-  /* USER CODE BEGIN Init */
+	/* USER CODE BEGIN Init */
+	char str[80] = "test\n";
+	char str_baro[80] = "test\n";
+	int overflows_to_Vy_calc = 10;
+	double altitude = 0;
+	double verticalSpeed = 0;
+	uint32_t ers_servo_set_up_position = 1600;
 
-  /* USER CODE END Init */
+	/* USER CODE END Init */
 
-  /* Configure the system clock */
-  SystemClock_Config();
+	/* Configure the system clock */
+	SystemClock_Config();
 
-  /* USER CODE BEGIN SysInit */
+	/* USER CODE BEGIN SysInit */
 
-  /* USER CODE END SysInit */
+	/* USER CODE END SysInit */
 
-  /* Initialize all configured peripherals */
-  MX_GPIO_Init();
-  MX_TIM2_Init();
-  MX_USART2_UART_Init();
-  MX_TIM3_Init();
-  MX_TIM5_Init();
-  MX_ADC1_Init();
-  MX_I2C1_Init();
-  MX_ADC2_Init();
-  MX_I2C3_Init();
-  MX_TIM6_Init();
-  /* USER CODE BEGIN 2 */
-  HAL_TIM_RegisterCallback(&htim2, HAL_TIM_IC_CAPTURE_CB_ID, IcHandlerTim2);
+	/* Initialize all configured peripherals */
+	MX_GPIO_Init();
+	MX_TIM2_Init();
+	MX_USART2_UART_Init();
+	MX_TIM3_Init();
+	MX_TIM5_Init();
+	MX_ADC1_Init();
+	MX_I2C1_Init();
+	MX_ADC2_Init();
+	MX_I2C3_Init();
+	MX_TIM6_Init();
+	/* USER CODE BEGIN 2 */
+	HAL_TIM_RegisterCallback(&htim2, HAL_TIM_IC_CAPTURE_CB_ID, IcHandlerTim2);
 	HAL_TIM_RegisterCallback(&htim5, HAL_TIM_IC_CAPTURE_CB_ID, IcHandlerTim5);
 
 	HAL_TIM_IC_Start_IT(&htim2, TIM_CHANNEL_1);//PA5 thr input
@@ -174,57 +187,67 @@ int main(void)
 	HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_4);//PB1 ail servo 2 output
 	HAL_TIM_PWM_Start(&htim5, TIM_CHANNEL_4);//PA3 rud servo 2 output
 	HAL_TIM_PWM_Start(&htim5, TIM_CHANNEL_3);//PA2 ers servo 2 output
+	
+	Servo 	thr_servo(htim3.Instance, 1),
+			elev_servo(htim3.Instance, 2),
+			ail_servo_1(htim3.Instance, 3),
+			ail_servo_2(htim3.Instance, 4),
+			rud_servo(htim5.Instance, 4),
+			ers_servo(htim5.Instance, 3);
 
-	char str[32];
-	Servo 	thr_servo(htim3.Instance, 1), elev_servo(htim3.Instance, 2),
-			ail_servo_1(htim3.Instance, 3), ail_servo_2(htim3.Instance, 4),
-			rud_servo(htim5.Instance, 4), ers_servo(htim5.Instance, 3);
-
-	uint32_t ers_servo_set_up_position = 1600;
 	ers_servo.setPositionMicroSeconds(ers_servo_set_up_position);
 
 	Beeper beeper(GPIOD, GPIO_PIN_13);
-  /* USER CODE END 2 */
+	MS5611 ms5611(0x77, hi2c1, 100, overflows_to_Vy_calc);//нельзя инитить до инита i2c
+	/* USER CODE END 2 */
 
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
-  while (1)
-  {
-	while(Armed(&beeper))
+	/* Infinite loop */
+	/* USER CODE BEGIN WHILE */
+	while (1)
 	{
-		thr_servo.setPositionMicroSeconds(thr_rc.getPulseWidth());
+		while(Armed(&beeper))
+		{
+			thr_servo.setPositionMicroSeconds(thr_rc.getPulseWidth());
+			elev_servo.setPositionMicroSeconds(elev_rc.getPulseWidthDif());
+			ail_servo_1.setPositionMicroSeconds(ail_rc.getPulseWidthDif());
+			ail_servo_2.setPositionMicroSeconds(ail_rc.getPulseWidthDif());
+			rud_servo.setPositionMicroSeconds(rud_rc.getPulseWidth());
+		}
+		while(ERS())
+		{
+			thr_servo.setPositionMicroSeconds(thr_rc.getChannelMinWidth());
+			beeper.longBeep();													//уже дает 1000Мс чтобы винт остановился до выпуска парашюта
+			ers_servo.setPositionMicroSeconds(540);								//(540 - 1600 мкс диапазон дивжения планки САС)
+			beeper.seriesBeep();
+		}
 		elev_servo.setPositionMicroSeconds(elev_rc.getPulseWidthDif());
 		ail_servo_1.setPositionMicroSeconds(ail_rc.getPulseWidthDif());
 		ail_servo_2.setPositionMicroSeconds(ail_rc.getPulseWidthDif());
 		rud_servo.setPositionMicroSeconds(rud_rc.getPulseWidth());
-	}
-	while(ERS())
-	{
 		thr_servo.setPositionMicroSeconds(thr_rc.getChannelMinWidth());
-		beeper.longBeep();													//уже дает 1000Мс чтобы винт остановился до выпуска парашюта
-		ers_servo.setPositionMicroSeconds(540);								//(540 - 1600 мкс диапазон дивжения планки САС)
-		beeper.seriesBeep();
+		ers_servo.setPositionMicroSeconds(slider_rc.getPulseWidth() - 448);
+
+		//чтение данных:
+		altitude = ms5611.getRawAltitude();
+
+		#ifdef SERVO_DEBUG_UART
+			HAL_UART_Transmit(&huart2, (uint8_t*)str, sprintf(str, "%d ", thr_rc.getPulseWidth()), 1000);
+			HAL_UART_Transmit(&huart2, (uint8_t*)str, sprintf(str, "%d ", elev_rc.getPulseWidth()), 1000);
+			HAL_UART_Transmit(&huart2, (uint8_t*)str, sprintf(str, "%d ", ail_rc.getPulseWidth()), 1000);
+			HAL_UART_Transmit(&huart2, (uint8_t*)str, sprintf(str, "%d ", rud_rc.getPulseWidth()), 1000);
+			HAL_UART_Transmit(&huart2, (uint8_t*)str, sprintf(str, "%d ", switch_rc.getPulseWidth()), 1000);
+			HAL_UART_Transmit(&huart2, (uint8_t*)str, sprintf(str, "%d\n", slide_rc.getPulseWidth()), 1000);
+			HAL_Delay(500);
+		#endif
+		#ifdef SENSOR_DEBUG_UART
+			HAL_UART_Transmit(&huart2, (uint8_t*)str, sprintf(str, "%d\n", (int) (altitude * 100)), 1000);
+			HAL_Delay(500);
+		#endif
+		/* USER CODE END WHILE */
+
+		/* USER CODE BEGIN 3 */
 	}
-	elev_servo.setPositionMicroSeconds(elev_rc.getPulseWidthDif());
-	ail_servo_1.setPositionMicroSeconds(ail_rc.getPulseWidthDif());
-	ail_servo_2.setPositionMicroSeconds(ail_rc.getPulseWidthDif());
-	rud_servo.setPositionMicroSeconds(rud_rc.getPulseWidth());
-	thr_servo.setPositionMicroSeconds(thr_rc.getChannelMinWidth());
-	ers_servo.setPositionMicroSeconds(slider_rc.getPulseWidth() - 448);
-
-	#ifdef DEBUG_UART
-	HAL_UART_Transmit(&huart2, (uint8_t*)str, sprintf(str, "%d ", thr_rc.getPulseWidth()), 1000);
-	HAL_UART_Transmit(&huart2, (uint8_t*)str, sprintf(str, "%d ", elev_rc.getPulseWidth()), 1000);
-	HAL_UART_Transmit(&huart2, (uint8_t*)str, sprintf(str, "%d ", ail_rc.getPulseWidth()), 1000);
-	HAL_UART_Transmit(&huart2, (uint8_t*)str, sprintf(str, "%d ", rud_rc.getPulseWidth()), 1000);
-	HAL_UART_Transmit(&huart2, (uint8_t*)str, sprintf(str, "%d\n", switch_rc.getPulseWidth()), 1000);
-	HAL_Delay(500);
-	#endif
-    /* USER CODE END WHILE */
-
-    /* USER CODE BEGIN 3 */
-  }
-  /* USER CODE END 3 */
+		/* USER CODE END 3 */
 }
 
 /**
