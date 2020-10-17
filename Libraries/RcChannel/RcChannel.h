83e4513e13a1336e6c120b219dbae57ceddf3519
Pugach Alexander <fotone97@yandex.ru>
2020-10-18 01:20:28 +0300

    Добавлено начальное взведение ERS

--- Libraries/RcChannel/RcChannel.h
+++ Libraries/RcChannel/RcChannel.h
@@ -1,7 +1,7 @@
 #ifndef RCCHANNEL_H
 #define RCCHANNEL_H
 
-#include "stm32f4xx_hal.h"
+#include "stm32f4xx_hal.h"ч
 
 class RcChannel
 {
--- Logger/Core/Src/main.cpp
+++ Logger/Core/Src/main.cpp
@@ -133,6 +133,8 @@ int main(void)
 
   HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_1);
   Servo ersServo = Servo(htim2.Instance, 1, 530, 2460);
+  ersServo.Set_Position(150);
+
   HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_3);
   Servo engine = Servo(htim2.Instance, 3, 989, 2013);
 
