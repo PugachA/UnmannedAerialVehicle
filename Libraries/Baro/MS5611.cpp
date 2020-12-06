06e2b0b6e750f90e93121828e0b0b830cbd10005
Gavriliyuk Vasiliy <mlx-01@yandex.ru>
2020-12-06 12:45:58 +0300

    Изменение вычисления Vy
    Теперь вертикальная скорость считается из сырой высоты

--- Libraries/Baro/MS5611.cpp
+++ Libraries/Baro/MS5611.cpp
@@ -174,9 +174,9 @@ double MS5611::getQFEpressure(void)
 void MS5611::firstVsFilter(void)
 {
 	double error = 0;
-	//calcAltitude(); //uncomment this line and comment the next one if you want to use raw altitude
-	lpAltFilter();
-    error = k1*(this->filterAltitude - this->first_filter_output);
+	calcAltitude(); //uncomment this line and comment the next one if you want to use raw altitude, and change filteredAltitude to rawAltitude
+	//lpAltFilter();
+    error = k1*(this->rawAltitude - this->first_filter_output);
 	this->first_filter_output += error*dt;
 }
 
