ca0fd35f7d414b62b2aff86e40ec7096cab17d46
NoMonsters <33801216+NoMonsters@users.noreply.github.com>
2021-01-16 13:16:11 +0300

    Library for beta sensor added (#134)
    * Library for beta sensor added
    
    * math.h excluded
    
    * Сменил void методы на числовые
    
    convertADC записывает не в поле класса, а возвращает значение;
    calcAngle записывает не в поле класса, а возвращает значение;
    Переписал getRawData и getAngle под новые методы
    
    * Исправил присвоение дробного числа целочисленному типу
    
    Co-authored-by: Nikita Lituev <lituev.nikita@gmail.com>

---
+++ Libraries/Beta/P3002.cpp
@@ -0,0 +1,43 @@
+#include "P3002.h"
+
+P3002::P3002(ADC_HandleTypeDef hadc)
+{
+	this->hadc = hadc;
+	k_adc_to_angle = 0.001221;
+	b_adc_to_angle = -2.5;
+}
+
+uint32_t P3002::convertADC(void)
+{
+	uint32_t adc_raw = 0;
+
+	HAL_ADC_Start(&this->hadc);
+	HAL_ADC_PollForConversion(&this->hadc,100);
+	
+	adc_raw = HAL_ADC_GetValue(&this->hadc);
+	
+	HAL_ADC_Stop(&this->hadc);
+
+	return adc_raw;
+}
+
+uint32_t P3002::getRawData(void)
+{
+	return convertADC();
+}
+
+double P3002::calcAngle(void)
+{
+	uint32_t adc_raw = 0;
+	double angle = 0.0;
+
+	adc_raw = convertADC();
+	angle = k_adc_to_angle*double(adc_raw) + b_adc_to_angle;
+
+	return angle;
+}
+
+double P3002::getAngle(void)
+{
+	return calcAngle();
+}
---
+++ Libraries/Beta/P3002.h
@@ -0,0 +1,24 @@
+#ifndef _P3002_LIB_H_
+#define _P3002_LIB_H_
+
+#include "stm32f4xx_hal.h"
+class P3002
+{
+  private:
+	  ADC_HandleTypeDef hadc; //pointer to ADC
+	
+	  uint32_t convertADC(void);
+	  double calcAngle(void);
+	
+	  // characteristics coefficients
+	  double k_adc_to_angle;
+	  double b_adc_to_angle; // angle = k*adc_raw + b
+	  
+	  		
+	
+  public:
+	  P3002(ADC_HandleTypeDef);//constructor
+	  uint32_t getRawData(void);
+	  double getAngle(void);
+};
+#endif
