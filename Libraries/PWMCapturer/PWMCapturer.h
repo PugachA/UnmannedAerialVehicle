fe79b3d82e5872b32afcccd72400cb29597af73d
Gavriliyuk Vasiliy <mlx-01@yandex.ru>
2021-03-20 10:50:03 +0300

    Добавил в PWMcapture конструктор без макс и мин значений и ошибки замера

--- Libraries/PWMCapturer/PWMCapturer.h
+++ Libraries/PWMCapturer/PWMCapturer.h
@@ -24,6 +24,8 @@ class PWMCapturer
 				uint16_t min_value,
 				uint16_t max_value,
 				uint8_t measurement_error);
+		PWMCapturer(TIM_HandleTypeDef *htim,
+				uint8_t channel);
 		virtual ~PWMCapturer();
 		void calculatePulseWidth();
 		uint32_t getPulseWidthDif();
